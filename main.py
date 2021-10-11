7d5292a8fd3dbfd5bf8d17d39bbfe9bb531825c0
LuSP19 <48345486+LuSP19@users.noreply.github.com>
2021-10-11 14:30:59 +0500

    Added the ability to specify a data file and a template

--- main.py
+++ main.py
@@ -1,15 +1,39 @@
 import collections
 import datetime
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+from pathlib import Path
 
+import configargparse
 import pandas
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
 def main():
+    parser = configargparse.ArgParser(
+        description='Wine Shop command line application',
+    )
+    parser.add_argument(
+        '-d',
+        '--wines_data',
+        default='wines.xlsx',
+        env_var='WINE_SHOP_DATA_PATH',
+        help='path to Excel file containing information about wines',
+    )
+    parser.add_argument(
+        '-t',
+        '--template',
+        default='template.html',
+        env_var='WINE_SHOP_TEMPLATE_PATH',
+        help='path to template HTML file',
+    )
+    
+    args = parser.parse_args()
+    wines_data_file = args.wines_data
+    template_file = args.template
+    
     winery_foundation_year = 1920
     wines = pandas.read_excel(
-        'wine3.xlsx',
+        wines_data_file,
         sheet_name='Лист1',
         dtype={'Цена': int},
         keep_default_na=False
@@ -24,11 +48,11 @@ def main():
         sorted_wine_groups[key] = grouped_wines[key]
 
     env = Environment(
-        loader=FileSystemLoader('.'),
+        loader=FileSystemLoader(Path(template_file).parent),
         autoescape=select_autoescape(['html', 'xml']),
     )
 
-    template = env.get_template('template.html')
+    template = env.get_template(Path(template_file).name)
 
     rendered_page = template.render(
         wines=sorted_wine_groups,
